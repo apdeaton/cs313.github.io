f909f58ea23804275da0f482dfd626df2be041a0
Deaton-Alan <55462476+Deaton-Alan@users.noreply.github.com>
2020-01-29 09:13:28 -0700

    my db sql text file

---
+++ db/myDB.sql
@@ -0,0 +1,28 @@
+CREATE TABLE price (
+id SERIAL NOT NULL PRIMARY KEY,
+cost real NOT NULL);
+
+CREATE TABLE room (
+id SERIAL NOT NULL PRIMARY KEY,
+room_type varchar(256) NOT NULL,
+room_price INT NOT NULL REFERENCES price(id));
+
+CREATE TABLE cruise (
+id SERIAL NOT NULL PRIMARY KEY,
+cruise_type varchar(256) NOT NULL,
+room_price INT NOT NULL REFERENCES price(id));
+
+CREATE TABLE trip (
+vast-oasis-82184::DATABASE(> id SERIAL NOT NULL PRIMARY KEY,
+cruise_id INT NOT NULL REFERENCES cruise(id),
+room_id INT NOT NULL REFERENCES room(id),
+total_cost real NOT NULL);
+
+INSERT INTO price VALUES (1, 10.00);
+INSERT INTO price VALUES (2, 50.00);
+INSERT INTO price VALUES (3, 100.00);
+INSERT INTO price VALUES (4, 500.00);
+INSERT INTO price VALUES (5, 1000.00);
+INSERT INTO price VALUES (6, 1000.00);
+INSERT INTO price VALUES (7, 2000.00);
+INSERT INTO price VALUES (8, 3000.00);
