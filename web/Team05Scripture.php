efed8335b2a701489f088507eebf05d84c9c236e
Deaton-Alan <55462476+Deaton-Alan@users.noreply.github.com>
2020-02-04 16:43:54 -0700

    test

--- web/Team05Scripture.php
+++ web/Team05Scripture.php
@@ -29,7 +29,8 @@ print "<h1>Scripture Resources</h1>";
   
   foreach ($db->query('SELECT * FROM scripture') as $row)
   {
-   print "<p><a href='Team05ScriptureDetails.php'><b>$row[1] " . "$row[2]:" . "$row[3]</b></a></p>\n\n";
+   print "<p><a name='$row[0]' value='$row[4]' href='Team05ScriptureDetails.php'><b>$row[1] " . 
+   "$row[2]:" . "$row[3]</b></a></p>\n\n";
   }
   
 
