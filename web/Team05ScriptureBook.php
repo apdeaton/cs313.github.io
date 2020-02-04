41f38a5751b587c2d9170f2620a7db500f002912
Deaton-Alan <55462476+Deaton-Alan@users.noreply.github.com>
2020-02-04 16:24:00 -0700

    test

--- web/Team05ScriptureBook.php
+++ web/Team05ScriptureBook.php
@@ -36,7 +36,7 @@ print "<h1>Scripture Resources</h1>";
 
   
 
-  $query = "SELECT * FROM scripture WHERE book = '$book'";
+  $query = "SELECT * FROM scripture";// WHERE book = '$book'";
   foreach ($db->query($query) as $row)
   {
    print "<p><b>$row[1] " . "$row[2]:" . "$row[3]</b> - " . "\"$row[4]\"</p>\n\n";
