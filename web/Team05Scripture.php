a40f2e97f9c0805b1982272edbe6ed19dd3e4616
Deaton-Alan <55462476+Deaton-Alan@users.noreply.github.com>
2020-02-04 15:36:25 -0700

    Team 05 Scripture

--- web/Team05Scripture.php
+++ web/Team05Scripture.php
@@ -29,7 +29,7 @@ print "<h1>Scripture Resources</h1>";
   
   foreach ($db->query('SELECT * FROM scripture') as $row)
   {
-   print "<p><b>$row[1]" . "$row[2]:" . "$row[3]</b> - " . "$row[4]</p>\n\n";
+   print "<p><b>$row[1]" . "$row[2]:" . "$row[3]</b> - " . "\"$row[4]\"</p>\n\n";
   }
   
 
