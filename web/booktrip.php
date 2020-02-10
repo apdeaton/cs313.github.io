7dc7282cc0d0e02c6d1f91113ecd3badf2713ef4
Deaton-Alan <55462476+Deaton-Alan@users.noreply.github.com>
2020-02-10 16:55:41 -0700

    trip planner

--- web/booktrip.php
+++ web/booktrip.php
@@ -17,6 +17,8 @@ $dbUser = $dbopts["user"];
 $dbPassword = $dbopts["pass"];
 $dbName = ltrim($dbopts["path"],'/');
 
+//print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
+
 try {
  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 }
@@ -52,16 +54,18 @@ catch (PDOException $ex) {
 <header style="margin-left: 550px; color: rgb(0, 163, 228);">
   <h1>PLEASE FILL OUT OUR FORM</h1>
 </header>
-<div id="cruise" style="border-style: ridge;">
-  Select Cruise Type:
-  <select id="cars" style="width: 250px; height: 50px; font-size: 30px;">
+<div id="main" style="border-style: ridge;">
+Select Cruise Type:
+  <select id="cars" style="width: 250px; height: 25px; font-size: 15px; color: black">
+  <option value="none">Choose a Cruise</option>
     <option value="Salt Lake City">Salt Lake City Cruise -- $1000</option>
     <option value="Antarctic Cruise">Antarctic Cruise -- $2000</option>
     <option value="Moon Cruise">Moon Cruise -- $3000</option>
   </select><br>
 
   Select Room Type:
-  <select id="room" style="width: 250px; height: 50px; font-size: 30px;">
+  <select id="room" style="width: 250px; height: 25px; font-size: 15px; color: black;">
+  <option value="none2">Choose a Room</option>
     <option value="Sleep on Deck">Sleep on Deck -- $10</option>
     <option value="Half Room">Half Room -- $50</option>
     <option value="Normal-Sized Room">Normal-Sized Room -- $100</option>
@@ -69,11 +73,9 @@ catch (PDOException $ex) {
     <option value="Captain Quarters">Captain Quarters -- $1000</option>
   </select><br>
 
-
-  <br>
-  <p style="font-size: 20px;">
-    <button type="button" class="btn btn-default" onclick="window.location.href='trip.php'">RETURN HOME</button>  
-  </p>
+    <p style="font-size: 20px;">
+      <button type="button" class="btn btn-default" onclick="window.location.href='trip.php'">RETURN HOME</button>  
+    </p>
     
 </div>
 </body>
