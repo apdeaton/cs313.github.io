90e7fcf0b15ab4ad279e15c5f7da6860ffaf8c60
Deaton-Alan <55462476+Deaton-Alan@users.noreply.github.com>
2020-02-10 16:53:15 -0700

    trip planner

--- web/viewtrip.php
+++ web/viewtrip.php
@@ -55,7 +55,21 @@ catch (PDOException $ex) {
   <h1>YOUR CURRENT BOOKINGS</h1>
 </header>
 <div id="main" style="border-style: ridge;">
-  TO BE FILLED OUT LATER
+Select Cruise Type:
+  <select id="cars" style="width: 250px; height: 50px; font-size: 30px;">
+    <option value="Salt Lake City">Salt Lake City Cruise -- $1000</option>
+    <option value="Antarctic Cruise">Antarctic Cruise -- $2000</option>
+    <option value="Moon Cruise">Moon Cruise -- $3000</option>
+  </select><br>
+
+  Select Room Type:
+  <select id="room" style="width: 250px; height: 50px; font-size: 30px;">
+    <option value="Sleep on Deck">Sleep on Deck -- $10</option>
+    <option value="Half Room">Half Room -- $50</option>
+    <option value="Normal-Sized Room">Normal-Sized Room -- $100</option>
+    <option value="Luxury Suite">Luxury Suite -- $500</option>
+    <option value="Captain Quarters">Captain Quarters -- $1000</option>
+  </select><br>
 
     <p style="font-size: 20px;">
       <button type="button" class="btn btn-default" onclick="window.location.href='trip.php'">RETURN HOME</button>  
