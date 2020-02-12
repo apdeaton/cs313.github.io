07c12f98a4667398fb4fd81045b865e2ec823d01
Deaton-Alan <55462476+Deaton-Alan@users.noreply.github.com>
2020-02-11 18:54:09 -0700

    Team06 Scripture Link

--- web/booktrip.php
+++ web/booktrip.php
@@ -51,7 +51,7 @@ $roomCostQuery = "SELECT cost FROM trip AS t JOIN room AS r ON t.cruise_id = r.i
 JOIN price AS p ON r.room_price = p.id WHERE room_id = $room";
 
 $cruiseCost = $db->query($cruiseCostQuery);
-print "<br> $cruiseCost";
+print $cruiseCost;
 //$roomCost = $db->query($roomCostQuery);
 
 //$totalCost = $cruiseCost + $roomCost;
