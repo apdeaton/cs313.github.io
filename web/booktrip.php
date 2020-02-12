82e7c1dcd0d997b29e609791757e003b88d7a38f
Deaton-Alan <55462476+Deaton-Alan@users.noreply.github.com>
2020-02-11 20:29:03 -0700

    Team06 Scripture Link

--- web/booktrip.php
+++ web/booktrip.php
@@ -89,7 +89,7 @@ print "total cost:  $totalCost";
   
 
 
-$tripQuery = "INSERT INTO trip (cruise_id, room_id) VALUES ($cruise, $room)";
+//$tripQuery = "INSERT INTO trip (cruise_id, room_id) VALUES ($cruise, $room)";
 
 $stmt = $db->prepare("INSERT INTO trip (cruise_id, room_id, total_cost) VALUES ($cruise, $room, $totalCost)");
 //$stmt->bindValue(':cruise', $cruise, PDO::PARAM_INT);
