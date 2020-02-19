92ceb1118addd18f1a05095944fbcf2afa02a261
Deaton-Alan <55462476+Deaton-Alan@users.noreply.github.com>
2020-02-19 13:19:43 -0700

    site

--- web/viewtrip.php
+++ web/viewtrip.php
@@ -84,7 +84,18 @@ function deleteTrip() {
         $query = "SELECT cruise_type, room_type, total_cost, trip_id FROM trip AS t JOIN cruise 
         AS c ON t.cruise_id = c.id JOIN room AS r ON t.room_id = r.id";
         foreach ($db->query($query) as $row) {
-          print "<p><b>CRUISE: </b>$row[0]<br> <b>ROOM: </b>" .  
+          print "<p><b>CRUISE: </b>$row[0]<br> 
+          if ($row[0] == 1) {
+            print '<img src='slccruise.jpg'><br><br>';
+          }
+          else if ($row[0] == 2) {
+            print '<img src='antarcticcruise.jpg'><br><br>';
+          }
+          else if ($row[0] == 3) {
+            print '<img src='mooncruise.jpg'><br><br>';
+          }
+          <br>
+          <b>ROOM: </b>" .  
           "$row[1]<br> <b>TOTAL COST: $</b>" . "$row[2] </p>
           <p> 
           
